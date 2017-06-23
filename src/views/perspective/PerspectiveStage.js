38d4c8a45f2caf737305008bddbffcda277ae9d0
AntonVolt <antonvoltchok1@gmail.com>
2017-06-23 19:39:12 -0400

    created Perspective Camera / Stage / Viewport , MoveTool, Perspective3dContainer

---
+++ src/views/perspective/PerspectiveStage.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+export default class PerspectiveStage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+
+    return (
+      <div>
+        container
+      </div>
+    );
+  }
+}
+
+
--- src/views/perspective/PerspectiveViewport.js
+++ src/views/perspective/PerspectiveViewport.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 
-export default class PerspectiveCamera extends Component {
+export default class PerspectiveViewport extends Component {
 
   constructor(props) {
     super(props);
